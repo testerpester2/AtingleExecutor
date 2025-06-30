4038ccc64bba332ecd6b2d76813e507257b4a898
AtingleTeam <teemismint@gmail.com>
2025-06-30 13:36:45 +0100

    Delete sober_test_inject.c

--- sober_test_inject.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-__attribute__((constructor)) void init() {
-    FILE *f = fopen("/tmp/sober_inject.log", "a");
-    if (f) {
-        fprintf(f, "Sober injected successfully.\n");
-        fclose(f);
-    }
-}
