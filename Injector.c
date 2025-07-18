c73e13c15e1b7e3d519b58709a73497f55961f1c
AtingleTeam <218103176+AtingleTeam@users.noreply.github.com>
2025-07-18 17:24:30 +0100

    Update Injector.c

--- Injector.c
+++ Injector.c
@@ -11,15 +11,15 @@
 #include <errno.h>
 #include <limits.h>
 #include <fcntl.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
 
 unsigned long get_base_address(pid_t pid, const char *module) {
     char path[PATH_MAX];
     snprintf(path, sizeof(path), "/proc/%d/maps", pid);
 
     FILE *f = fopen(path, "r");
-    if (!f) {
-        return 0;
-    }
+    if (!f) return 0;
 
     char line[256];
     while (fgets(line, sizeof(line), f)) {
@@ -36,9 +36,7 @@ unsigned long get_base_address(pid_t pid, const char *module) {
 
 unsigned long get_local_offset(const char *lib_path, const char *symbol) {
     void *handle = dlopen(lib_path, RTLD_LAZY);
-    if (!handle) {
-        return 0;
-    }
+    if (!handle) return 0;
 
     void *symbol_addr = dlsym(handle, symbol);
     if (!symbol_addr) {
@@ -59,9 +57,7 @@ unsigned long get_local_offset(const char *lib_path, const char *symbol) {
 
 pid_t find_pid(const char *target_name) {
     DIR* d = opendir("/proc");
-    if (!d) {
-        return -1;
-    }
+    if (!d) return -1;
 
     struct dirent* e;
     while ((e = readdir(d))) {
@@ -75,9 +71,7 @@ pid_t find_pid(const char *target_name) {
         snprintf(exe_path, sizeof(exe_path), "/proc/%d/exe", pid);
 
         ssize_t len = readlink(exe_path, link_target, sizeof(link_target) - 1);
-        if (len == -1) {
-            continue;
-        }
+        if (len == -1) continue;
         link_target[len] = '\0';
 
         if (strstr(link_target, target_name)) {
@@ -88,46 +82,71 @@ pid_t find_pid(const char *target_name) {
     closedir(d);
     return -1;
 }
-// should work better for now
-int inject(pid_t pid, const char* library_path) {
-    unsigned long remote_libc_base = get_base_address(pid, "libc.so.6");
-    if (remote_libc_base == 0) {
-        return 0;
-    }
 
-    unsigned long remote_libdl_base = get_base_address(pid, "libdl.so.2");
-    if (remote_libdl_base == 0) {
-        return 0;
+int ptrace_write_data(pid_t pid, unsigned long addr, void *data, size_t size) {
+    unsigned long i;
+    long word;
+    for (i = 0; i < size; i += sizeof(long)) {
+        memcpy(&word, (char*)data + i, (size - i < sizeof(long)) ? (size - i) : sizeof(long));
+        ptrace(PTRACE_POKEDATA, pid, addr + i, word);
     }
+    return 0;
+}
+
+int inject(pid_t pid, const char* library_path) {
+    struct user_regs_struct old_regs, regs;
+    int status;
+
+    ptrace(PTRACE_ATTACH, pid, NULL, NULL);
+    waitpid(pid, &status, 0);
+
+    ptrace(PTRACE_GETREGS, pid, NULL, &old_regs);
+    memcpy(&regs, &old_regs, sizeof(struct user_regs_struct));
+
+    unsigned long remote_libc_base = get_base_address(pid, "libc.so.6");
+    unsigned long remote_libdl_base = get_base_address(pid, "libdl.so.2");
 
     unsigned long mmap_offset = get_local_offset("libc.so.6", "mmap");
-    if (mmap_offset == 0) {
-        return 0;
-    }
     unsigned long remote_mmap_addr = remote_libc_base + mmap_offset;
 
     unsigned long dlopen_offset = get_local_offset("libdl.so.2", "dlopen");
-    if (dlopen_offset == 0) {
-        return 0;
-    }
     unsigned long remote_dlopen_addr = remote_libdl_base + dlopen_offset;
 
     size_t library_path_len = strlen(library_path) + 1;
-    size_t total_alloc_size = (library_path_len + 0xFFF + 0x1000) & ~0xFFF;
+    size_t alloc_size = (library_path_len + 0xFFF) & ~0xFFF;
 
-    void* remote_mem_placeholder = (void*)0x000000; // Placeholder
+    regs.rip = remote_mmap_addr;
+    regs.rdi = 0; // let kernel choose
+    regs.rsi = alloc_size;
+    regs.rdx = PROT_READ | PROT_WRITE | PROT_EXEC;
+    regs.rcx = MAP_PRIVATE | MAP_ANONYMOUS;
+    regs.r8 = -1;
+    regs.r9 = 0;
 
-    if (remote_mem_placeholder == NULL || remote_mem_placeholder == MAP_FAILED) {
-        return 0;
-    }
+    ptrace(PTRACE_SETREGS, pid, NULL, &regs);
+    ptrace(PTRACE_CONT, pid, NULL, NULL);
+    waitpid(pid, &status, 0);
 
-    struct iovec local_path_io = { (void*)library_path, library_path_len };
-    struct iovec remote_path_io = { remote_mem_placeholder, library_path_len };
-    if (process_vm_writev(pid, &local_path_io, 1, &remote_path_io, 1, 0) == -1) {
-        return 0;
-    }
+    ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+    unsigned long remote_allocated_mem = regs.rax;
 
-    void* remote_shellcode_addr = remote_mem_placeholder + library_path_len; // mmap and dlopen
+    unsigned long remote_lib_path_addr = remote_allocated_mem;
+    ptrace_write_data(pid, remote_lib_path_addr, (void*)library_path, library_path_len);
+
+    regs.rip = remote_dlopen_addr;
+    regs.rdi = remote_lib_path_addr; // filename
+    regs.rsi = RTLD_NOW; // flags
+
+    ptrace(PTRACE_SETREGS, pid, NULL, &regs);
+    ptrace(PTRACE_CONT, pid, NULL, NULL);
+    waitpid(pid, &status, 0);
+
+    ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+    unsigned long remote_lib_handle = regs.rax; // Not used
+
+    ptrace(PTRACE_SETREGS, pid, NULL, &old_regs);
+
+    ptrace(PTRACE_DETACH, pid, NULL, NULL);
 
     return 1;
 }
