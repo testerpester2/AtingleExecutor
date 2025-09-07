f06b6502492ed1b23d78d4fa9f6295aa411e91a5
testerpester2 <gumgettergaminglol@gmail.com>
2025-09-07 19:46:59 -0400

    Changed build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
+rm -rf build
+mkdir build
 gcc Injector.c -o build/injector -ldl
 gcc AtingleUI.c -o build/AtingleUI `pkg-config --cflags --libs gtk4` -lpthread
+gcc sober_test_inject.c -o build/sober_test_inject.so -shared -fPIC -ldl
+gcc injected_lib.c -o build/injected_lib.so -shared -fPIC -ldl
+echo "Build complete. Executables are in the 'build' directory."
